22cf085703b2812325014c046b3c434854cacfc2
omrlns <marlon.da.silva996@gmail.com>
2026-01-05 19:39:18 -0300

    feat(java): exemplos com processamento de dados, atribuição e casting

---
+++ processamento-de-dados/src/com/marlon/processamentodedados/Main.java
@@ -0,0 +1,56 @@
+package com.marlon.processamentodedados;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int valorA, valorB;
+
+        valorA = 5;
+        valorB = 5 * valorA;
+
+        System.out.println(valorA);
+        System.out.println(valorB);
+        System.out.println();
+
+        int valorC;
+        double valorD;
+
+        valorC = 10;
+        valorD = 10 * valorC;
+
+        System.out.println(valorC);
+        System.out.println(valorD);
+        System.out.println();
+
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);
+        System.out.println();
+
+        int numero1, numero2;
+        double resultado;
+
+        numero1 = 10;
+        numero2 = 2;
+
+        resultado = (double) numero1 / numero2;
+
+        System.out.println(resultado);
+        System.out.println();
+
+        double valorY;
+        int valorZ;
+
+        valorY = 10;
+        valorZ = (int) valorY;
+
+        System.out.println(valorZ);
+
+    }
+}
