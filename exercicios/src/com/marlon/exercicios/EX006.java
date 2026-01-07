9280b86b9792c0fa6c0972b46fb9621819ba989b
omrlns <marlon.da.silva996@gmail.com>
2026-01-07 17:20:29 -0300

    feat(java): resolução do EX006

---
+++ exercicios/src/com/marlon/exercicios/EX006.java
@@ -0,0 +1,18 @@
+package com.marlon.exercicios;
+
+import java.util.Scanner;
+
+public class EX006 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int valor = sc.nextInt();
+
+        if (valor < 0) {
+            System.out.println("O NÚMERO É NEGATIVO!");
+        } else {
+            System.out.println("O NÚMERO É POSITIVO!");
+        }
+        sc.close();
+    }
+}
