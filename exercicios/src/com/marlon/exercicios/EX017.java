619547834750de4ce5b1aac2f9338c84838c640d
omrlns <marlon.da.silva996@gmail.com>
2026-01-15 16:11:52 -0300

    feat(java): resolução do EX017

---
+++ exercicios/src/com/marlon/exercicios/EX017.java
@@ -0,0 +1,21 @@
+// leia um valor inteiro. em seguida mostre os ímpares de 1 até X.
+
+package com.marlon.exercicios;
+
+import java.util.Scanner;
+
+public class EX017 {
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int valorX = sc.nextInt();
+
+        for (int i = 0; i <= valorX; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
