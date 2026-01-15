aad3f87498257588f418600fe2ce0ef9482f34e0
omrlns <marlon.da.silva996@gmail.com>
2026-01-15 15:36:15 -0300

    feat(java): aprendendo estrutura repetitiva com for

---
+++ estrutura-repetitiva/src/com/marlon/repetitiva/For.java
@@ -0,0 +1,23 @@
+package com.marlon.repetitiva;
+
+import java.util.Scanner;
+
+public class For {
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int nValores = sc.nextInt();
+        int soma = 0;
+
+        for (int i = 0; i < nValores; i += 1) {
+            int valorX = sc.nextInt();
+            soma += valorX;
+        }
+
+
+        System.out.printf("A SOMA DOS VALORES DIGITADOS É: %d", soma);
+        sc.close();
+
+    }
+}
